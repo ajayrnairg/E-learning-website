8a8ced9402d71932ce12e24739613dc9e91e0270
guru24102001 <gurujnn@gmail.com>
2021-02-07 16:57:50 +0530

    Teacher dashboard, home, sign up and sign in added

--- E2(Education made easy)/app.js
+++ E2(Education made easy)/app.js
@@ -39,7 +39,10 @@ const moduleSchema = {
 };
 const loginSchema1 = {
 	username: String,
-	pass: String
+	pass: String,
+	email: String,
+  address: String,
+  institution: String
 
 };
 
@@ -128,11 +131,81 @@ app.post("/", function(req,res){
 	});
 	}
 });
+app.get("/teacher/home", function(req,res){
 
+	res.render("teacherhome");
+});
+
+
+app.post("/teacher/home", function(req,res){
+	var m = req.body.signup;
+	var m1 = req.body.login;
+	if(m === "signup")
+	{
+		Teacher.exists({username: req.body.uname , email: req.body.email}, function(err,doce){
+			if(doce === true)
+			{
+				alert("Account already existsplease login");
+				res.redirect("/teacher/home");
+			}
+			else if(doce === false)
+			{
+	    var salt = "Xy";
+	    var hashed = crypto.createHash('md5').update(req.body.pass).digest("hex");
+        var passhash = hashed + salt;
+        const login1 = new Teacher ({
+        username: req.body.uname,
+        pass: passhash,
+		email: req.body.email,
+		address: req.body.addr,
+		institution: req.body.insttt,
+
+    });
+    login1.save(function(err){
+    if (!err){
+	   console.log("Success inserted account");
+    }
+	res.redirect("/teacher/home");
+    });
+	}
+	});
+	}
+	if(m1 === "login")
+	{
+		var salt = "Xy";
+	var hashed = crypto.createHash('md5').update(req.body.pass).digest("hex");
+   var passhash = hashed + salt;
+	var name = req.body.uname;
+	const pass= req.body.pass;
+	if(req.body.uname<1 && req.body.pass<1)
+	{
+		alert("Please enter details");
+	}
+	Teacher.exists({username: name , pass: passhash}, function(err,doc){
+		if(err){
+			console.log(err);
+			console.log("Invalid username or password");
+		}
+		else{
+			if(doc===true){
+				req.session.username1 = req.body.uname;
+				req.session.pass1 = req.body.pass;
+				res.redirect("/teacher/dashboard");
+			}
+			else if(doc===false)
+			{
+					alert("Invalid username or password or enterned invalid plan");
+				console.log("Invalid username or password or enterned invalid plan");
+				res.redirect("/teacher/home");
+			}
+		}
+	});
+	}
+});
 
 app.get("/dashboard", function(req,res){
 	if(req.session.username)
-	{	
+	{
 	res.render("dashboard", {name: req.session.username});
 	}
 	else{
@@ -149,3 +222,21 @@ app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
+app.get("/teacher/dashboard", function(req,res){
+	if(req.session.username1){
+		Student.find({}, function(err , postsse){
+		console.log("hh");
+	    res.render("teacherdashboard", {students: postsse});
+	    });
+	}
+	else{
+		res.send("Not logged in");
+	}
+});
+app.get("/progress/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  Student.findOne({_id: requestedPostId}, function(err, postsss){
+   req.session.username = postsss.username;
+   res.redirect("/dashboard");
+   });
+ });
