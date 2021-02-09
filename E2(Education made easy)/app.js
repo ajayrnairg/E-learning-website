3066aaa47bd260959c8878a911cc078112484d73
Shubham-Zope <2019shubham.zope@ves.ac.in>
2021-02-09 17:27:33 +0530

    Counselling and doubt

--- E2(Education made easy)/app.js
+++ E2(Education made easy)/app.js
@@ -57,6 +57,15 @@ const counselingSchema = {
 	description: String,
 	time: String
 }
+
+const doubtsolvingSchema = {
+	studentusername: String,
+	teacherusername: String,
+	doubt: String,
+	reply: String
+}
+
+
 const rankingSchema = {
 	username: String,
 	rankss: Number 
@@ -67,7 +76,7 @@ const Module = mongoose.model("Module",moduleSchema);
 const Teacher = mongoose.model("Teacher", loginSchema1);
 const Counseling = mongoose.model("Counseling", counselingSchema);
 const Ranking = mongoose.model("Ranking", rankingSchema);
-
+const Doubt = mongoose.model("Doubt", doubtsolvingSchema);
 app.get("/math", function(req,res){
 	if(req.session.username)
 	{
@@ -86,7 +95,9 @@ app.get("/", function(req,res){
 app.get("/module4", function(req,res){
 	if(req.session.username)
 	{
-	res.render("module4");
+	Module.findOne({username: req.session.username , module: "Module1"}, function(err , poste){
+		res.render("module4", {score: poste.quizresult , attm: poste.attempts});
+	});
 	}
 	else{
 		res.send("Not log in");
@@ -96,10 +107,46 @@ app.get("/module4", function(req,res){
 app.get("/module7", function(req,res){
 	if(req.session.username)
 	{
-	res.render("module7");
+		Module.findOne({username: req.session.username , module: "Module2"}, function(err , poste){
+	res.render("module7", {score: poste.quizresult , attm: poste.attempts});
+		});
 	}
 	else{
 		res.send("Not log in");
+	}
+});
+
+app.get("/studentcounseling", function(req , res){
+	if(req.session.username)
+	{
+		if(req.session.plan === "paid")
+		{
+		Teacher.find({}, function(err , posttea){
+		console.log("hh");
+		console.log("postea");
+	    res.render("studentcounseling", {teachers: posttea});
+	    });
+		}
+	}
+	else{
+		res.send("Nt log in");
+	}
+});
+
+app.get("/studentdoubt", function(req , res){
+	if(req.session.username)
+	{
+		if(req.session.plan === "paid")
+		{
+		Teacher.find({}, function(err , posttea){
+		console.log("hh");
+		console.log("postea");
+	    res.render("studentdoubt", {teachers: posttea});
+	    });
+		}
+	}
+	else{
+		res.send("Nt log in");
 	}
 });
 
@@ -235,12 +282,45 @@ app.get("/module2quiz", function(req,res){
 app.get("/module1answerquiz", function(req,res){
 	if(req.session.username)
 	{
-        res.render("quiz2answerkey");		
+        res.render("quiz2answerkey", {question1r: req.session.question1 , question2r: req.session.question2
+		, question3r: req.session.question3 , question4r: req.session.question4
+		, question5r: req.session.question5 , question6r: req.session.question6
+		, question7r: req.session.question7 , question8r: req.session.question8 
+		, question9r: req.session.question9 , question10r: req.session.question10});		
 	}
 	else{
 		res.send("Not log in");
 	}
 });
+
+app.get("/module1freeanswerquiz", function(req,res){
+	if(req.session.username)
+	{
+        res.render("quiz2freeanswer", {question1r: req.session.question1 , question2r: req.session.question2
+		, question3r: req.session.question3 , question4r: req.session.question4
+		, question5r: req.session.question5 , question6r: req.session.question6
+		, question7r: req.session.question7 , question8r: req.session.question8 
+		, question9r: req.session.question9 , question10r: req.session.question10});		
+	}
+	else{
+		res.send("Not log in");
+	}
+});
+
+app.get("/module2freeanswerquiz", function(req,res){
+	if(req.session.username)
+	{
+        res.render("module7freeanswer", {question1r: req.session.question1 , question2r: req.session.question2
+		, question3r: req.session.question3 , question4r: req.session.question4
+		, question5r: req.session.question5 , question6r: req.session.question6
+		, question7r: req.session.question7 , question8r: req.session.question8 
+		, question9r: req.session.question9 , question10r: req.session.question10});		
+	}
+	else{
+		res.send("Not log in");
+	}
+});
+
 
 app.get("/mathfree", function(req,res){
 	if(req.session.username){
@@ -253,7 +333,9 @@ app.get("/mathfree", function(req,res){
 
 app.get("/module4free", function(req,res){
 	if(req.session.username){
-		res.render("module4free");
+		Module.findOne({username: req.session.username , module: "Module1"}, function(err , postse){
+			res.render("module4free", {score: postse.quizresult , attm: postse.attempts});
+		});
 	}
 	else{
 		res.send("Not log in");
@@ -262,7 +344,9 @@ app.get("/module4free", function(req,res){
 
 app.get("/module7free", function(req,res){
 	if(req.session.username){
-		res.render("module7free");
+		Module.findOne({username: req.session.username , module: "Module2"}, function(err , postse){
+			res.render("module7free", {score: postse.quizresult , attm: postse.attempts});
+		});
 	}
 	else{
 		res.send("Not log in");
@@ -271,7 +355,11 @@ app.get("/module7free", function(req,res){
 
 app.get("/module7answer", function(req,res){
 	if(req.session.username){
-		res.render("module7answer");
+		res.render("module7answer" , {question1r: req.session.question1 , question2r: req.session.question2
+		, question3r: req.session.question3 , question4r: req.session.question4
+		, question5r: req.session.question5 , question6r: req.session.question6
+		, question7r: req.session.question7 , question8r: req.session.question8 
+		, question9r: req.session.question9 , question10r: req.session.question10});
 	}
 	else{
 		res.send("Not log in");
@@ -609,42 +697,52 @@ app.post("/module1quiz", function(req,res){
 	if(qmodone1 === "25200")
 	{
 		c++;
+		req.session.question1 = qmodone1;
 	}
 	if(qmodone2 === "209")
 	{
 		c++;
+		req.session.question2 = qmodone2;
 	}
 	if(qmodone3 === "720")
 	{
 		c++;
+		req.session.question3 = qmodone3;
 	}
 	if(qmodone4 === "50400")
 	{
 		c++;
+		req.session.question4 = qmodone4;
 	}
 	if(qmodone5 === "63")
 	{
 		c++;
+		req.session.question5 = qmodone5;
 	}
 	if(qmodone6 === "120960")
 	{
 		c++;
+		req.session.question6 = qmodone6;
 	}
 	if(qmodone7 === "11760")
 	{
 		c++;
+		req.session.question7 = qmodone7;
 	}
 	if(qmodone8 === "720")
 	{
 		c++;
+		req.session.question8 = qmodone8;
 	}
 	if(qmodone9 === "8")
 	{
 		c++;
+		req.session.question9 = qmodone9;
 	}
 	if(qmodone10 === "36")
 	{
 		c++;
+		req.session.question10 = qmodone10;
 	}
 	console.log(c);
 	req.session.quizr = c;
@@ -686,42 +784,52 @@ app.post("/module1freequiz", function(req,res){
 	if(qmodone1 === "25200")
 	{
 		c++;
+		req.session.question1 = qmodone1;
 	}
 	if(qmodone2 === "209")
 	{
 		c++;
+		req.session.question2 = qmodone2;
 	}
 	if(qmodone3 === "720")
 	{
 		c++;
+		req.session.question3 = qmodone3;
 	}
 	if(qmodone4 === "50400")
 	{
 		c++;
+		req.session.question4 = qmodone4;
 	}
 	if(qmodone5 === "63")
 	{
 		c++;
+		req.session.question5 = qmodone5;
 	}
 	if(qmodone6 === "120960")
 	{
 		c++;
+		req.session.question6 = qmodone6;
 	}
 	if(qmodone7 === "11760")
 	{
 		c++;
+		req.session.question7 = qmodone7;
 	}
 	if(qmodone8 === "720")
 	{
 		c++;
+		req.session.question8 = qmodone8;
 	}
 	if(qmodone9 === "8")
 	{
 		c++;
+		req.session.question9 = qmodone9;
 	}
 	if(qmodone10 === "36")
 	{
 		c++;
+		req.session.question10 = qmodone10;
 	}
 	console.log(c);
 	req.session.quizr = c;
@@ -744,7 +852,7 @@ app.post("/module1freequiz", function(req,res){
       }
     });
 	});
-	res.redirect("/module1answerquiz");
+	res.redirect("/module1freeanswerquiz");
 });
 
 app.post("/module2quiz", function(req,res){
@@ -763,42 +871,52 @@ app.post("/module2quiz", function(req,res){
 	if(qmodone1 === "1/26")
 	{
 		c++;
+		req.session.question1 = qmodone1;
 	}
 	if(qmodone2 === "2/91")
 	{
 		c++;
+		req.session.question2= qmodone2;
 	}
 	if(qmodone3 === "13/102")
 	{
 		c++;
+		req.session.question3 = qmodone3;
 	}
 	if(qmodone4 === "9/52")
 	{
 		c++;
+		req.session.question4 = qmodone4;
 	}
 	if(qmodone5 === "4/7")
 	{
 		c++;
+		req.session.question5 = qmodone5;
 	}
 	if(qmodone6 === "3/4")
 	{
 		c++;
+		req.session.question6 = qmodone6;
 	}
 	if(qmodone7 === "21/46")
 	{
 		c++;
+		req.session.question7 = qmodone7;
 	}
 	if(qmodone8 === "2/7")
 	{
 		c++;
+		req.session.question8 = qmodone8;
 	}
 	if(qmodone9 === "1/221")
 	{
 		c++;
+		req.session.question9 = qmodone9;
 	}
 	if(qmodone10 === "5/12")
 	{
 		c++;
+		req.session.question10 = qmodone10;
 	}
 	console.log(c);
 	req.session.quizr = c;
@@ -840,42 +958,52 @@ app.post("/module2freequiz", function(req,res){
 	if(qmodone1 === "1/26")
 	{
 		c++;
+		req.session.question1 = qmodone1;
 	}
 	if(qmodone2 === "2/91")
 	{
 		c++;
+		req.session.question2= qmodone2;
 	}
 	if(qmodone3 === "13/102")
 	{
 		c++;
+		req.session.question3 = qmodone3;
 	}
 	if(qmodone4 === "9/52")
 	{
 		c++;
+		req.session.question4 = qmodone4;
 	}
 	if(qmodone5 === "4/7")
 	{
 		c++;
+		req.session.question5 = qmodone5;
 	}
 	if(qmodone6 === "3/4")
 	{
 		c++;
+		req.session.question6 = qmodone6;
 	}
 	if(qmodone7 === "21/46")
 	{
 		c++;
+		req.session.question7 = qmodone7;
 	}
 	if(qmodone8 === "2/7")
 	{
 		c++;
+		req.session.question8 = qmodone8;
 	}
 	if(qmodone9 === "1/221")
 	{
 		c++;
+		req.session.question9 = qmodone9;
 	}
 	if(qmodone10 === "5/12")
 	{
 		c++;
+		req.session.question10 = qmodone10;
 	}
 	console.log(c);
 	req.session.quizr = c;
@@ -898,7 +1026,7 @@ app.post("/module2freequiz", function(req,res){
       }
     });
 	});
-	res.redirect("/module7answer");
+	res.redirect("/module2freeanswerquiz");
 });
 
 app.get("/logout", function(req,res){
@@ -923,6 +1051,82 @@ app.get("/teacher/dashboard", function(req,res){
 		res.send("Not logged in");
 	}
 });
+
+app.get("/ttcounseling", function(req,res){
+		Counseling.find({}, function(err , posttacoun){
+		console.log("hh");
+		console.log(posttacoun);
+	    res.render("ttcounseling", {counselingsss: posttacoun});
+	    });
+});
+
+app.get("/doubtall", function(req,res){
+		Doubt.find({}, function(err , posttacoun){
+		console.log("hh");
+		console.log(posttacoun);
+	    res.render("doubtall", {counselingsss: posttacoun});
+	    });
+});
+
+app.get("/teacher/counselling", function(req,res){
+	if(req.session.username1)
+	{
+	Counseling.find({}, function(err , postteacoun){
+		console.log("hh");
+		console.log(postteacoun);
+	    res.render("teachercounseling", {counselingss: postteacoun});
+	    });
+	}
+	else{
+		res.send("Not log in");
+	}
+});
+
+app.get("/teacher/doubt", function(req,res){
+	if(req.session.username1)
+	{
+	Doubt.find({}, function(err , postteacoun){
+		console.log("hh");
+		console.log(postteacoun);
+	    res.render("teacherdoubt", {counselingss: postteacoun});
+	    });
+	}
+	else{
+		res.send("Not log in");
+	}
+});
+
+app.post("/teacher/counselling", function(req,res){
+	req.session.username = req.body.studentsub;
+	req.session.description = req.body.description;
+	console.log(req.body.time);
+	var tm = req.body.time;
+	console.log("I am"+tm);
+	Counseling.findOneAndUpdate({description: req.body.description}, {time: tm}, function(err, foundListss){
+      if (!err){
+		  foundListss.save();
+		  console.log("success updated time");
+      }
+    });
+	res.redirect("/ttcounseling");
+});
+
+app.post("/teacher/doubt", function(req,res){
+	req.session.username = req.body.studentsub;
+	req.session.description = req.body.description;
+	console.log(req.body.reply);
+	var tm = req.body.reply;
+	console.log("I am"+tm);
+	Doubt.findOneAndUpdate({doubt: req.body.description}, {reply: tm}, function(err, foundListss){
+      if (!err){
+		  foundListss.save();
+		  console.log("success updated reply");
+      }
+    });
+	res.redirect("/doubtall");
+});
+
+
 app.get("/progress/:postId", function(req, res){
   const requestedPostId = req.params.postId;
   Student.findOne({_id: requestedPostId}, function(err, postsss){
@@ -930,3 +1134,33 @@ app.get("/progress/:postId", function(req, res){
    res.redirect("/dashboard");
    });
  });
+ 
+ app.post("/studentcounseling", function(req,res){
+	console.log(req.body.studentsubmit);
+	req.session.username1 = req.body.studentsubmit;
+	const coun = new Counseling({
+	 studentusername: req.session.username,
+	 teacherusername: req.body.studentsubmit,
+	 description: req.body.description,
+	 time: "0"
+ });
+ coun.save(function(err){
+	console.log("Success inserted counselling");
+ });
+ res.redirect("/teacher/counselling");
+});
+
+ app.post("/studentdoubt", function(req,res){
+	console.log(req.body.studentsubmit);
+	req.session.username1 = req.body.studentsubmit;
+	const coun1 = new Doubt({
+	 studentusername: req.session.username,
+	 teacherusername: req.body.studentsubmit,
+	 doubt: req.body.description,
+	 reply: "0"
+ });
+ coun1.save(function(err){
+	console.log("Success inserted counselling");
+ });
+ res.redirect("/teacher/doubt");
+});
